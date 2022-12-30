c11fb36581f4ca50ddfc23cf2f8a0aa20485e45f
Anastasiia Dubyna <anastasiia.dubyna@dataart.com>
2022-12-30 11:25:11 +0100

    more carousel progress

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import { Card, Tab, Tabs } from "@mui/material";
+import { Tab, Tabs } from "@mui/material";
 import { nanoid } from "nanoid";
 import PropTypes from 'prop-types';
 import "./carousel.css";
@@ -8,19 +8,24 @@ const Carousel = ({title, tabs, media, activeTab, onTabChange}) => {
     if (media) {
         return (
             <div className="carousel">
-                <div className="carousel-title">{ title }</div>
-                <Tabs
-                    value={activeTab}
-                    onChange={onTabChange}
-                >
-                    {
-                        tabs.map(tab => <Tab label={tab} value={tab} key={nanoid(3)} />)
-                    }
-                </Tabs>
+                <div className="carousel-header">
+                    <div className="carousel-title">{ title }</div>
+                    <Tabs
+                        value={activeTab}
+                        onChange={onTabChange}
+                    >
+                        {
+                            tabs.map(tab => <Tab label={tab} value={tab} key={nanoid(3)} />)
+                        }
+                    </Tabs>
+                </div>
                 <div className="carousel-media">
                     {
                         media.map(mediaData => (
-                            <Card key={nanoid(4)}>{mediaData.title}</Card>
+                            <div key={nanoid(4)} className="media-card">
+                                <img className="media-image" src={`https://image.tmdb.org/t/p/w154${mediaData.poster_path}`} />
+                                <p className="media-title">{mediaData.title || mediaData.name}</p>
+                            </div>
                         ))
                     }
                 </div>
--- src/repository/movieRepository.js
+++ src/repository/movieRepository.js
@@ -10,15 +10,11 @@ export const getMovieById = async (id) => {
 
 export const getTrending = async (type) => {
     const {data: {results}} = await fetchData(`${API_URL}/trending/all/${type}`);
-    getImage();
+    getConfigurations();
     return results;
 };
 
-export const getImage = async () => {
-    // const {data: {posters}} = await fetchData("https://api.themoviedb.org/3/movie/550/images");
-    // console.log(posters);
-    // const {data: {images: {base_url}}} = await fetchData(`${API_URL}/configuration`);
-    // console.log(base_url);
-    const response = await fetchData("https://image.tmdb.org/t/p/w342/cdkyMYdu8ao26XOBvilNzLneUg1.jpg");
-    console.log(response);
+const getConfigurations = async () => {
+    const {data} = await fetchData(`${API_URL}/configuration`);
+    console.log(data);
 };
