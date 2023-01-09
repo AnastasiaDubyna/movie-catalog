012be8b0a94f1078836b6be25ea3e3847ca1ccc8
Anastasiia Dubyna <anastasiia.dubyna@dataart.com>
2023-01-09 11:00:50 +0100

    footer added

---
+++ src/components/footer/constants.js
@@ -0,0 +1,6 @@
+export const footerContent = [
+    ["THE BASICS", "About TMDB", "Contact Us", "Support Forums", "API", "System Status"],
+    ["GET INVOLVED", "Contribution Bible", "Add New Movie", "Add New TV Show"],
+    ["COMMUNITY", "Guidelines", "Discussions", "Leaderboard", "Twitter"],
+    ["LEGAL", "Terms of Use", "API Terms of Use", "Privacy Policy"]
+];
