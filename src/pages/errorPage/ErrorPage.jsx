d034ef431c0270e8c48300fe2b7fe42e07f90e53
Anastasiia Dubyna <anastasiia.dubyna@dataart.com>
2023-02-01 14:01:37 +0100

    optimistic update added

---
+++ src/pages/errorPage/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => (
+    <div className="error-page-content">
+        <h1>Oops an error occurred</h1>
+        <h2>Try again later</h2>
+    </div>
+    // Я потом оформлю это адекватнее 
+);
+
+export default ErrorPage;
